5141f123f9299a492c99abdee273d460f558020f
Adil Khan <ksaquib54@gmail.com>
2020-08-30 21:01:56 +0530

    first deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+const path = require("path");
 
 const app = express();
 
@@ -22,14 +23,14 @@ app.use("/api/register", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 
 //Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//Set static folder
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
 
-//   app.use("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
